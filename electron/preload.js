76c62e26c0427e7c4cf7b48d4935467e8a7b5258
RonnieHarrod-cell <ronnieharrod43@gmail.com>
2026-04-13 22:49:36 +0100

    chore: update UI components and electron config

--- electron/preload.js
+++ electron/preload.js
@@ -1,28 +1,51 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electron', {
-    keystore: {
-        get: () => ipcRenderer.invoke('keystore:get'),
-        set: (key) => ipcRenderer.invoke('keystore:set', key),
-    },
+  keystore: {
+    get: () => ipcRenderer.invoke('keystore:get'),
+    set: (key) => ipcRenderer.invoke('keystore:set', key),
+  },
 
-    // Window controls
-    window: {
-        minimize: () => ipcRenderer.send('window:minimize'),
-        maximize: () => ipcRenderer.send('window:maximize'),
-        close: () => ipcRenderer.send('window:close'),
-    },
+  window: {
+    minimize: () => ipcRenderer.send('window:minimize'),
+    maximize: () => ipcRenderer.send('window:maximize'),
+    close:    () => ipcRenderer.send('window:close'),
+  },
 
-    // Menu-triggered actions (main → renderer)
-    onMenuAction: (callback) => {
-        const events = ['menu:run', 'menu:stop', 'menu:save', 'menu:fork', 'menu:new-prompt']
-        const listeners = events.map(channel => {
-            const handler = (_, ...args) => callback(channel.replace('menu:', ''), ...args)
-            ipcRenderer.on(channel, handler)
-            return { channel, handler }
-        })
-        return () => listeners.forEach(({ channel, handler }) => ipcRenderer.removeListener(channel, handler))
+  updater: {
+    download: () => ipcRenderer.invoke('updater:download'),
+    install:  () => ipcRenderer.invoke('updater:install'),
+    check:    () => ipcRenderer.invoke('updater:check'),
+    onChange: (callback) => {
+      const events = [
+        'updater:checking',
+        'updater:available',
+        'updater:not-available',
+        'updater:progress',
+        'updater:downloaded',
+        'updater:error',
+        'updater:manual-check',
+      ]
+      const listeners = events.map(channel => {
+        const handler = (_, data) => callback(channel.replace('updater:', ''), data)
+        ipcRenderer.on(channel, handler)
+        return { channel, handler }
+      })
+      return () => listeners.forEach(({ channel, handler }) =>
+        ipcRenderer.removeListener(channel, handler))
     },
+  },
 
-    platform: process.platform,
+  onMenuAction: (callback) => {
+    const events = ['menu:run', 'menu:stop', 'menu:save', 'menu:fork', 'menu:new-prompt']
+    const listeners = events.map(channel => {
+      const handler = (_, ...args) => callback(channel.replace('menu:', ''), ...args)
+      ipcRenderer.on(channel, handler)
+      return { channel, handler }
+    })
+    return () => listeners.forEach(({ channel, handler }) =>
+      ipcRenderer.removeListener(channel, handler))
+  },
+
+  platform: process.platform,
 })
